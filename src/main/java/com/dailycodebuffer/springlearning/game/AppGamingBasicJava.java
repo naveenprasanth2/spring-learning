aa7d6ab5502162f2270c35cc27c48527e0705194
naveen prasanth <naveenprasanth2@gmail.com>
2025-01-05 19:29:06 +0530

    added more class for checking the tight coupling

--- src/main/java/com/dailycodebuffer/springlearning/game/AppGamingBasicJava.java
+++ src/main/java/com/dailycodebuffer/springlearning/game/AppGamingBasicJava.java
@@ -4,7 +4,8 @@ package com.dailycodebuffer.springlearning.game;
 public class AppGamingBasicJava {
     public static void main(String[] args) {
 //        var game = new MarioGame();
-        var game = new SuperContraGame();
+        var game = new PackMan();
+//        var game = new SuperContraGame();
         var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
